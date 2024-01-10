32c05b08d53805656bf33cffc8571d2ae595a861
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-01-10 14:38:28 +0300

    Initial setup of dashboard-layout

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,29 +1,52 @@
-import React, { Fragment } from "react";
+import React, { Fragment, ReactNode } from "react";
 import { Routes, Route } from "react-router-dom";
 import { SignUp } from "../auth/UI/SignUp";
 import { SignIn } from "../auth/UI/SignIn";
 import { ForgotPassword } from "../auth/UI/ForgotPassword";
 import { ResetPassword } from "../auth/UI/ResetPassword";
 
+interface Page {
+  name: string;
+  icon: ReactNode;
+  path: string;
+  element: ReactNode;
+}
+
+interface Route {
+  title: string;
+  pages: Page[];
+}
+
 export const AuthRoutes: React.FC = () => {
-  const routes = [
-    {
-      element: <SignUp />,
-      path: "/signup",
-    },
-    {
-      element: <SignIn />,
-      path: "/signin",
-    },
-    {
-      element: <ForgotPassword />,
-      path: "/forgot-password",
-    },
-    {
-      element: <ResetPassword />,
-      path: "/reset-password",
-    },
-  ];
+  const routes: Route[] = {
+    title: "auth",
+    pages: [
+      {
+        name: "Sign up",
+        icon: "icon",
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        name: "Sign up",
+        icon: "icon",
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        name: "Sign up",
+        icon: "icon",
+        path: "/forgot-password",
+        element: <ForgotPassword />,
+      },
+      {
+        name: "Sign up",
+        icon: "icon",
+        path: "/reset-password",
+        element: <ResetPassword />,
+      },
+    ],
+  };
 
   return (
     <Fragment>
--- src/shared/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from "react";
-
-export const Header: React.FC = () => {
-  return (
-    <Fragment>
-      <div>Header</div>
-    </Fragment>
-  );
-};
