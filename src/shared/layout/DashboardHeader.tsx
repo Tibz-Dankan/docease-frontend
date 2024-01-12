cb6134ec6d94c94ef194f6603c294397f83a1451
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-01-12 23:11:30 +0300

    refactor: refine the header

--- src/shared/layout/DashboardHeader.tsx
+++ src/shared/layout/DashboardHeader.tsx
@@ -1,19 +1,15 @@
-import { useLocation, Link } from "react-router-dom";
-import {
-  // Navbar,
-  Typography,
-  IconButton,
-  Breadcrumbs,
-} from "@material-tailwind/react";
-import { Bars3Icon } from "@heroicons/react/24/solid";
 import { openSidebarHandler } from "../../store/actions/sidebar";
 import { useDispatch } from "react-redux";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoIosMenu } from "react-icons/io";
+import { PiChatsCircleLight } from "react-icons/pi";
+import { IoPerson } from "react-icons/io5";
+import { GoTriangleDown } from "react-icons/go";
+import { IconContext } from "react-icons";
+import { SearchApp } from "../UI/SearchApp";
+import logo from "../../assets/images/logo.jpeg";
 
-export function DashboardHeader() {
-  const fixedNavbar = true;
-  const { pathname } = useLocation();
-  const [layout, page] = pathname.split("/").filter((el) => el !== "");
-
+export const DashboardHeader = () => {
   const dispatch: any = useDispatch();
 
   const handleOpenSidebar = () => {
@@ -26,48 +22,63 @@ export function DashboardHeader() {
       z-40 py-3 h-16 bg-primary"
     >
       <div className="flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center">
-        <div className="capitalize">
-          <Breadcrumbs
-            className={`bg-transparent p-0 transition-all ${
-              fixedNavbar ? "mt-1" : ""
-            }`}
-            placeholder={""}
-          >
-            <Link to={`/${layout}`}>
-              <Typography
-                variant="small"
-                color="blue-gray"
-                className="font-normal opacity-50 transition-all hover:text-blue-500 hover:opacity-100"
-                placeholder={""}
-              >
-                {layout}
-              </Typography>
-            </Link>
-            <Typography
-              variant="small"
-              color="blue-gray"
-              className="font-normal"
-              placeholder={""}
-            >
-              {page}
-            </Typography>
-          </Breadcrumbs>
-          <Typography variant="h6" color="blue-gray" placeholder={""}>
-            {page}
-          </Typography>
+        <div className="flex items-center gap-0 relative bg-purple-500s">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-12 absolute -top-1 left-0 fill-white"
+          />
+          <span className="text-gray-50 ml-11 text-2xl">Docease</span>
         </div>
+
         <div className="flex items-center">
-          <IconButton
-            variant="text"
-            color="blue-gray"
-            className="grid xl:hidden"
+          <SearchApp />
+        </div>
+
+        <div className="flex items-center gap-4">
+          <span
             onClick={() => handleOpenSidebar()}
-            placeholder={""}
+            className="inline-block xl:hidden cursor-pointer"
           >
-            <Bars3Icon strokeWidth={3} className="h-6 w-6 text-blue-gray-500" />
-          </IconButton>
+            <IconContext.Provider value={{ size: "1.8rem", color: "#fff" }}>
+              <IoIosMenu />
+            </IconContext.Provider>
+          </span>
+          <span
+            onClick={() => handleOpenSidebar()}
+            className="inline-block cursor-pointer"
+          >
+            <IconContext.Provider value={{ size: "1.8rem", color: "#fff" }}>
+              <PiChatsCircleLight />
+            </IconContext.Provider>
+          </span>
+          <span
+            onClick={() => handleOpenSidebar()}
+            className="inline-block cursor-pointer"
+          >
+            <IconContext.Provider value={{ size: "1.8rem", color: "#fff" }}>
+              <IoMdNotificationsOutline />
+            </IconContext.Provider>
+          </span>
+          <div className="flex items-center">
+            <span
+              className="cursor-pointer grid place-items-center  bg-gray-700 p-1
+            w-10 h-10 rounded-[50%]"
+            >
+              <IconContext.Provider
+                value={{ size: "1.2rem", color: "#ced4da" }}
+              >
+                <IoPerson />
+              </IconContext.Provider>
+            </span>
+            <span className="inline-block cursor-pointer">
+              <IconContext.Provider value={{ size: "1.2rem", color: "#fff" }}>
+                <GoTriangleDown />
+              </IconContext.Provider>
+            </span>
+          </div>
         </div>
       </div>
     </header>
   );
-}
+};
