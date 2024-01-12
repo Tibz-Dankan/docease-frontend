1167bd2f22a9ff3e7a832b07499ed469827a548d
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-01-12 18:11:05 +0300

    fix: correct attrbute casing

--- src/shared/UI/Loader.tsx
+++ src/shared/UI/Loader.tsx
@@ -17,9 +17,9 @@ export const Loader: React.FC<LoaderProps> = (props) => {
           stroke="#fff"
           className={twMerge("text-gray-600", props.className)}
         >
-          <g fill="none" fill-rule="evenodd">
+          <g fill="none" fillRule="evenodd">
             <g transform="translate(1 1)" strokeWidth="2">
-              <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
+              <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
               <path d="M36 18c0-9.94-8.06-18-18-18">
                 <animateTransform
                   attributeName="transform"
