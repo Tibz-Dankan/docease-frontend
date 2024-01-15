2511fec448feff8fdbbeabf2270b23979931eda5
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-01-15 12:04:16 +0300

    update backend url for production

--- src/store/index.ts
+++ src/store/index.ts
@@ -15,7 +15,7 @@ let url: string;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   url = "http://localhost:8000/api/v1";
 } else {
-  url = "production-url/api/v1";
+  url = "https://docease-backend-dd99.onrender.com/api/v1";
 }
 
 export { url };
