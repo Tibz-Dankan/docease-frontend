0fe9ad4fd38d837fa20e1220969e63c92cb2d047
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-02-03 13:24:17 +0300

    feat: add heading for file list

--- src/medical-history/UI/MedicalFileList.tsx
+++ src/medical-history/UI/MedicalFileList.tsx
@@ -9,6 +9,12 @@ export const MedicalFileList: React.FC = () => {
   return (
     <Fragment>
       <div className="w-full space-y-4">
+        <div
+          className=" w-full text-gray-800s text-center
+          bg-gray-300 rounded-md p-4 text-primary"
+        >
+          <p>Uploaded Files</p>
+        </div>
         {records.map((record, index: number) => (
           <div key={index}>
             <FileItem filename={record.name} createdAt={record.createdAt} />
--- src/medical-history/layout/MedicalLayout.tsx
+++ src/medical-history/layout/MedicalLayout.tsx
@@ -12,21 +12,26 @@ export const MedicalLayout: React.FC<MedicalLayoutProps> = (props) => {
         <div className="flex flex-col justify-center">
           <div
             className="flex items-center justify-center 
-             border-b-[1px] border-gray-300 p-4 -mt-6 text-lg
+             border-b-[1px]s border-gray-300 p-2 -mt-6 text-lg
              text-gray-800 font-semibold"
           >
             <p>Medical History</p>
           </div>
           <div
             className="flex items-center justify-center 
-             border-b-[1px] border-gray-300 p-4 gap-8
-             text-gray-800"
+             border-b-[1px] border-gray-300 p-2 gap-8
+             text-gray-700 -mt-4 text-sm"
           >
             <span>
-              <Link to="/patient/medical-history#files">Medical Files</Link>
+              <Link
+                to="/patient/medical-history#files"
+                className="text-primary"
+              >
+                Files
+              </Link>
             </span>
             <span>
-              <Link to="/patient/medical-history#form">Medical Form</Link>
+              <Link to="/patient/medical-history#form">Form</Link>
             </span>
           </div>
         </div>
