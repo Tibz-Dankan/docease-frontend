43e0f903771c444556cd33e2274b396d0492a61a
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-03-18 13:27:52 +0300

    refactor: improve styles change password form

--- src/settings/UI/ChangePassword.tsx
+++ src/settings/UI/ChangePassword.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { TAuthState } from "../../types/auth";
 import { updatePassword } from "../API";
@@ -9,25 +9,23 @@ import {
 } from "../../store/actions/notification";
 import { Button } from "../../shared/UI/Button";
 import { Loader } from "../../shared/UI/Loader";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { InputField } from "../../shared/UI/InputField";
+
+type TPasswords = {
+  currentPassword: string;
+  newPassword: string;
+  confirmNewPassword: string;
+};
 
 export const ChangePassword: React.FC = () => {
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmNewPassword, setConfirmNewPassword] = useState("");
-
   const accessToken = useSelector(
     (state: TAuthState) => state.auth.accessToken
   ) as string;
   const userId = useSelector(
     (state: TAuthState) => state.auth.user?.userId
   ) as string;
-
-  const currentPasswordChangeHandler = (event: any) =>
-    setCurrentPassword(() => event.target.value);
-  const newPasswordChangeHandler = (event: any) =>
-    setNewPassword(() => event.target.value);
-  const confirmNewPasswordChangeHandler = (event: any) =>
-    setConfirmNewPassword(() => event.target.value);
 
   const dispatch: any = useDispatch();
 
@@ -49,79 +47,92 @@ export const ChangePassword: React.FC = () => {
     },
   });
 
-  const changePasswordHandler = async (event: any) => {
-    event.preventDefault();
-    if (
-      !currentPassword ||
-      !newPassword ||
-      newPassword !== confirmNewPassword
-    ) {
-      console.log("form has errors");
-      return;
-    }
-    mutate({
-      userId: userId,
-      currentPassword: currentPassword,
-      newPassword: newPassword,
-      accessToken: accessToken,
-    });
+  const initialValues: TPasswords = {
+    currentPassword: "",
+    newPassword: "",
+    confirmNewPassword: "",
   };
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: Yup.object({
+      currentPassword: Yup.string()
+        .max(255)
+        .required("Current password is required"),
+      newPassword: Yup.string().max(255).required("New password is required"),
+      confirmNewPassword: Yup.string()
+        .max(255)
+        .required("Confirm password is required"),
+    }),
+
+    onSubmit: async (values, helpers) => {
+      try {
+        if (!values.currentPassword || !values.newPassword) {
+          console.log("form has errors");
+          return;
+        }
+        if (values.newPassword !== values.confirmNewPassword) {
+          formik.errors.confirmNewPassword = "Passwords don't much!";
+          dispatch(
+            showCardNotification({
+              type: "error",
+              message: "Please check form for errors",
+            })
+          );
+          setTimeout(() => {
+            dispatch(hideCardNotification());
+          }, 5000);
+
+          return;
+        }
+        mutate({
+          userId: userId,
+          currentPassword: values.currentPassword,
+          newPassword: values.newPassword,
+          accessToken: accessToken,
+        });
+      } catch (err: any) {
+        helpers.setStatus({ success: false });
+        helpers.setSubmitting(false);
+        dispatch(showCardNotification({ type: "error", message: err.message }));
+        setTimeout(() => {
+          dispatch(hideCardNotification());
+        }, 5000);
+      }
+    },
+  });
 
   return (
     <div className="w-full h-full grid place-items-center">
       <form
-        onSubmit={changePasswordHandler}
-        className="w-[80%] h-fulls space-y-4
-           bg-gray-50 rounded-md p-12"
+        onSubmit={formik.handleSubmit}
+        className="w-[90%] sm:w-[480px] space-y-4s bg-white
+        rounded-md shadow-md p-12"
       >
-        <div className="flex flex-col justify-center gap-[6px]">
-          <label htmlFor="Full Name" className="text-gray-800 text-sm">
-            Current password
-          </label>
-          <input
-            type="password"
-            value={currentPassword}
-            onChange={currentPasswordChangeHandler}
-            placeholder="Enter current password"
-            required
-            className="w-full p-3 px-4 rounded placeholder:text-gray-500
-              focus:border-primary  focus:border-[1px] outline-none 
-              transition-all border-[1px] border-gray-300 border-b-[3px] 
-              shadow-sm"
-          />
-        </div>
-        <div className="flex flex-col justify-center gap-[6px]">
-          <label htmlFor="Full Name" className="text-gray-800 text-sm">
-            New password
-          </label>
-          <input
-            type="password"
-            value={newPassword}
-            onChange={newPasswordChangeHandler}
-            placeholder="Enter new password"
-            required
-            className="w-full p-3 px-4 rounded placeholder:text-gray-500
-                focus:border-primary  focus:border-[1px] outline-none 
-                transition-all border-[1px] border-gray-300 border-b-[3px] 
-                shadow-sm"
-          />
-        </div>
-        <div className="flex flex-col justify-center gap-[6px]">
-          <label htmlFor="Full Name" className="text-gray-800 text-sm">
-            Confirm New password
-          </label>
-          <input
-            type="password"
-            value={confirmNewPassword}
-            onChange={confirmNewPasswordChangeHandler}
-            placeholder="Confirm new password"
-            required
-            className="w-full p-3 px-4 rounded placeholder:text-gray-500
-                focus:border-primary  focus:border-[1px] outline-none 
-                transition-all border-[1px] border-gray-300 border-b-[3px] 
-                shadow-sm"
-          />
-        </div>
+        <p
+          className="text-gray-700 text-xl font-semibold
+            text-center"
+        >
+          Change your password
+        </p>
+        <InputField
+          type="password"
+          label="Current Password"
+          name="currentPassword"
+          formik={formik}
+        />
+        <InputField
+          type="password"
+          label="New Password"
+          name="newPassword"
+          formik={formik}
+        />
+        <InputField
+          type="password"
+          label="Confirm New Password"
+          name="confirmNewPassword"
+          formik={formik}
+        />
         <div className="flex items-center justify-start pt-6">
           {!isLoading && <Button label="Change" type="submit" />}
           {isLoading && <Loader />}
