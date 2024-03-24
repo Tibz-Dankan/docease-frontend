c442ae42c28692c4c5205dc61e8e81a2d9547242
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-03-24 19:20:41 +0300

    refactor: improve background of forgotPassword, resetPassword and verifyTwoFAToken forms

--- src/auth/UI/ForgotPassword.tsx
+++ src/auth/UI/ForgotPassword.tsx
@@ -13,6 +13,7 @@ import { forgotPassword } from "../API";
 import { Loader } from "../../shared/UI/Loader";
 import { Button } from "../../shared/UI/Button";
 import logo from "../../assets/images/logo.jpeg";
+import { SquareDots } from "../../common/UI/SquareDots";
 
 type TInitialValues = {
   email: string;
@@ -68,15 +69,46 @@ export const ForgotPassword: React.FC = () => {
 
   return (
     <Fragment>
-      <div className="min-h-screen grid place-items-center py-28">
+      <div
+        className="min-h-screen grid place-items-center py-28
+         relative bg-gray-200"
+      >
+        <div
+          className="bg-blue-100 w-[100vw] h-[60vh] 
+          absolute top-0 left-0 right-0 rounded-b-[60%] z-0"
+        />
+        <SquareDots
+          className="absolute bottom-[10vh] left-2 sm:left-6
+            xl:left-24 hidden sm:grid grid-cols-5 z-0"
+          size={"w-4 h-4"}
+          bgColor={""}
+          borderColor={"border-blue-100"}
+          gap={"gap-3"}
+          filled={false}
+          applyShadow={false}
+        />
+        <SquareDots
+          className="absolute bottom-[10vh] right-2 sm:right-6
+            xl:right-24 hidden sm:grid grid-cols-5 z-0"
+          size={"w-4 h-4"}
+          bgColor={""}
+          borderColor={"border-blue-100"}
+          gap={"gap-3"}
+          filled={false}
+          applyShadow={false}
+        />
         <form
           onSubmit={formik.handleSubmit}
           className="flex flex-col gap-0 items-center w-[90%] sm:w-[480px]
-          bg-blue-500s shadow-2xl p-8 rounded-2xl"
+          bg-gray-50 shadow-md p-8 rounded-md z-[1]"
         >
-          <img src={logo} alt="logo" className="w-28" />
-          <p className="text-center text-2xl font-semibold">Reset Password</p>
-          <p className="text-start text-sm text-gray-700 mt-4">
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <p className="text-center text-2xl font-semibold text-gray-800">
+            Reset Password
+          </p>
+          <p className="text-start text-sm md:text-base text-gray-700 mt-4">
             Please provide an email associated with your Docease account where
             we will send you a reset token.
           </p>
--- src/auth/UI/VerifyTwoFAToken.tsx
+++ src/auth/UI/VerifyTwoFAToken.tsx
@@ -14,6 +14,8 @@ import { Loader } from "../../shared/UI/Loader";
 import { Button } from "../../shared/UI/Button";
 import logo from "../../assets/images/logo.jpeg";
 import { authenticate } from "../../store/actions/auth";
+import { SquareDots } from "../../common/UI/SquareDots";
+import { Link } from "react-router-dom";
 
 type TTWoFAToken = {
   token: string;
@@ -64,18 +66,49 @@ export const VerifyTwoFAToken: React.FC = () => {
 
   return (
     <Fragment>
-      <div className="min-h-screen grid place-items-center py-28">
+      <div
+        className="min-h-screen grid place-items-center py-28
+         relative bg-gray-200"
+      >
+        <div
+          className="bg-blue-100 w-[100vw] h-[60vh] 
+          absolute top-0 left-0 right-0 rounded-b-[60%] z-0"
+        />
+        <SquareDots
+          className="absolute bottom-[10vh] left-2 sm:left-6
+            xl:left-24 hidden sm:grid grid-cols-5 z-0"
+          size={"w-4 h-4"}
+          bgColor={""}
+          borderColor={"border-blue-100"}
+          gap={"gap-3"}
+          filled={false}
+          applyShadow={false}
+        />
+        <SquareDots
+          className="absolute bottom-[10vh] right-2 sm:right-6
+            xl:right-24 hidden sm:grid grid-cols-5 z-0"
+          size={"w-4 h-4"}
+          bgColor={""}
+          borderColor={"border-blue-100"}
+          gap={"gap-3"}
+          filled={false}
+          applyShadow={false}
+        />
         <form
           onSubmit={formik.handleSubmit}
           className="flex flex-col gap-0 items-center w-[90%] sm:w-[480px]
-          bg-blue-500s shadow-2xl p-8 rounded-2xl text-gray-800"
+          bg-gray-50 shadow-md p-8 rounded-md z-[1]"
         >
-          <img src={logo} alt="logo" className="w-28" />
-          <p className="text-start text-2xl font-semibold">
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <p className="text-start text-2xl font-semibold text-gray-800">
             We need some security information
           </p>
-          <p className="text-center font-semibold my-2">Account Verification</p>
-          <p className="">
+          <p className="text-center font-semibold my-2 text-gray-800">
+            Account Verification
+          </p>
+          <p className="text-gray-700">
             If we patterns that seem unusual for your account, we will need to
             verify your identity
           </p>
