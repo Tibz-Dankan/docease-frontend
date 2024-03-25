cfa18ed5f1855dc49d525b40ba69f49d8dc88db9
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-03-25 10:02:43 +0300

    fix: make post appointment responsive

--- src/appointment/UI/PostAppointment.tsx
+++ src/appointment/UI/PostAppointment.tsx
@@ -15,6 +15,7 @@ import { Loader } from "../../shared/UI/Loader";
 import { DoctorScheduleList } from "../../schedule/UI/DoctorScheduleList";
 import { Schedule, ScheduleTime } from "../../types/schedule";
 import { AppointmentScheduleTimeSelect } from "./AppointmentScheduleTimeSelect";
+import { Modal } from "../../shared/UI/Modal";
 
 interface TileContentProps {
   date: any;
@@ -76,7 +77,6 @@ export const PostAppointment: React.FC<PostAppointmentProps> = (props) => {
   const { isLoading, mutate } = useMutation({
     mutationFn: postAppointment,
     onSuccess: (response: any) => {
-      console.log("response--->", response);
       dispatch(
         showCardNotification({
           type: "success",
@@ -146,125 +146,136 @@ export const PostAppointment: React.FC<PostAppointmentProps> = (props) => {
 
   return (
     <Fragment>
-      <div
-        className="p-8  w-[90%] md:w-[600px] h-[90v] 
-         md:h-[70vh] overflow-x-hidden flex flex-col gap-4"
-      >
-        <div className="text-sm text-gray-800">
-          <DoctorScheduleList
-            doctorId={props.doctorId}
-            doctorName={props.doctorName}
-            onFetch={onScheduleFetchHandler}
+      <Modal
+        openModalElement={
+          <Button
+            label="Make Appointment"
+            type="button"
+            className="w-full text-sm"
           />
-        </div>
+        }
+        className="w-[90vw] sm:w-[600px]"
+      >
         <div
-          className="text-base border-b-[1px] border-gray-300
-           py-2 -mt-4"
+          className="p-4 sm:p-8 w-full h-[70vh] overflow-x-hidden
+           flex flex-col gap-4"
         >
-          <p className="text-start text-gray-800 font-semibold">
-            Select Date and Time
-          </p>
-        </div>
-        <div className="flex flex-col sm:flex-row gap-4">
-          <div className="w-full">
-            <Calendar
-              onChange={appointmentDateChangeHandler}
-              value={appointmentDate}
-              className="w-full border-none bg-gray-300 px-0 text-lg
-               rounded h-[300]"
-              tileContent={tileContent}
-              tileClassName="relative"
+          <div className="text-sm text-gray-800">
+            <DoctorScheduleList
+              doctorId={props.doctorId}
+              doctorName={props.doctorName}
+              onFetch={onScheduleFetchHandler}
             />
           </div>
-          <div className="w-full">
-            <div className="flex flex-col justify-center items-start gap-[14px]">
-              <div className="w-full">
-                <p
-                  className="text-primary bg-gray-300 px-4 py-2 rounded
+          <div
+            className="text-base border-b-[1px] border-gray-300
+           py-2 -mt-4"
+          >
+            <p className="text-start text-gray-800 font-semibold">
+              Select Date and Time
+            </p>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-4">
+            <div className="w-full">
+              <Calendar
+                onChange={appointmentDateChangeHandler}
+                value={appointmentDate}
+                className="w-full border-none bg-gray-300 px-0 text-lg
+               rounded h-[300]"
+                tileContent={tileContent}
+                tileClassName="relative"
+              />
+            </div>
+            <div className="w-full">
+              <div className="flex flex-col justify-center items-start gap-[14px]">
+                <div className="w-full">
+                  <p
+                    className="text-primary bg-gray-300 px-4 py-2 rounded
                    w-full font-semibold text-center"
-                >
-                  {selectedDate}
-                </p>
-              </div>
-              <div className="flex flex-col items-start gap-1">
-                <label htmlFor="start-time" className="text-gray-800 text-sm">
-                  Start Time
-                </label>
-                {!hasScheduleTme && (
-                  <span
-                    className="bg-gray-300 outline-none focus:border-[2px]
-                    border-primary rounded p-2 text-primary text-sm"
                   >
-                    No Time Slot
-                  </span>
-                )}
-                {hasScheduleTme && (
-                  <AppointmentScheduleTimeSelect
-                    scheduleTimes={selectedScheduleTime}
-                    onSelectScheduleTime={onSelectScheduleTimeHandler}
-                    isStartTime={true}
-                    startTimeValue={appointmentStartTime}
-                    endTimeValue={appointmentEndTime}
-                  />
-                )}
-              </div>
-              <div className="flex flex-col items-start gap-1">
-                <label htmlFor="start-time" className="text-gray-800 text-sm">
-                  End Time
-                </label>
-                {!hasScheduleTme && (
-                  <span
-                    className="bg-gray-300 outline-none focus:border-[2px]
+                    {selectedDate}
+                  </p>
+                </div>
+                <div className="flex flex-col items-start gap-1">
+                  <label htmlFor="start-time" className="text-gray-800 text-sm">
+                    Start Time
+                  </label>
+                  {!hasScheduleTme && (
+                    <span
+                      className="bg-gray-300 outline-none focus:border-[2px]
                     border-primary rounded p-2 text-primary text-sm"
-                  >
-                    No Time Slot
-                  </span>
-                )}
-                {hasScheduleTme && (
-                  <AppointmentScheduleTimeSelect
-                    scheduleTimes={selectedScheduleTime}
-                    onSelectScheduleTime={onSelectScheduleTimeHandler}
-                    isEndTime={true}
-                    startTimeValue={appointmentStartTime}
-                    endTimeValue={appointmentEndTime}
-                  />
-                )}
-              </div>
-              <div className="w-full space-y-2">
-                <label htmlFor="subject" className="text-gray-800 text-sm">
-                  Subject
-                </label>
-                <textarea
-                  className="w-full bg-gray-300 border-[2px] outline-none
+                    >
+                      No Time Slot
+                    </span>
+                  )}
+                  {hasScheduleTme && (
+                    <AppointmentScheduleTimeSelect
+                      scheduleTimes={selectedScheduleTime}
+                      onSelectScheduleTime={onSelectScheduleTimeHandler}
+                      isStartTime={true}
+                      startTimeValue={appointmentStartTime}
+                      endTimeValue={appointmentEndTime}
+                    />
+                  )}
+                </div>
+                <div className="flex flex-col items-start gap-1">
+                  <label htmlFor="start-time" className="text-gray-800 text-sm">
+                    End Time
+                  </label>
+                  {!hasScheduleTme && (
+                    <span
+                      className="bg-gray-300 outline-none focus:border-[2px]
+                    border-primary rounded p-2 text-primary text-sm"
+                    >
+                      No Time Slot
+                    </span>
+                  )}
+                  {hasScheduleTme && (
+                    <AppointmentScheduleTimeSelect
+                      scheduleTimes={selectedScheduleTime}
+                      onSelectScheduleTime={onSelectScheduleTimeHandler}
+                      isEndTime={true}
+                      startTimeValue={appointmentStartTime}
+                      endTimeValue={appointmentEndTime}
+                    />
+                  )}
+                </div>
+                <div className="w-full space-y-2">
+                  <label htmlFor="subject" className="text-gray-800 text-sm">
+                    Subject
+                  </label>
+                  <textarea
+                    className="w-full bg-gray-300 border-[2px] outline-none
                   border-gray-300 rounded h-24 resize-none p-2
                   focus:border-primary text-sm transition-all"
-                  value={appointmentSubject}
-                  onChange={(event) =>
-                    setAppointmentSubject(() => event.target.value)
-                  }
-                />
+                    value={appointmentSubject}
+                    onChange={(event) =>
+                      setAppointmentSubject(() => event.target.value)
+                    }
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div>
-          {!isLoading && (
-            <Button
-              label="Make Appointment"
-              type="button"
-              onClick={() => appointmentPostHandler()}
-            />
-          )}
-          {isLoading && (
-            <div
-              className="bg-primary text-gray-50 flex items-center
+          <div>
+            {!isLoading && (
+              <Button
+                label="Make Appointment"
+                type="button"
+                onClick={() => appointmentPostHandler()}
+              />
+            )}
+            {isLoading && (
+              <div
+                className="bg-primary text-gray-50 flex items-center
                justify-center p-1 w-full rounded"
-            >
-              <Loader className="w-8 h-8" />
-            </div>
-          )}
+              >
+                <Loader className="w-8 h-8" />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      </Modal>
     </Fragment>
   );
 };
