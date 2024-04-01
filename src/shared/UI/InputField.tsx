9cb4bfe26bacf8ac653f4861751859ae24b99b01
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-04-01 23:17:40 +0300

    refactor: improve input field styles on focus

--- src/shared/UI/InputField.tsx
+++ src/shared/UI/InputField.tsx
@@ -11,20 +11,21 @@ export const InputField: React.FC<InputFieldProps> = (props) => {
   const formik = props.formik;
   const label = props.label;
   const name = props.name;
-  // TODO: To add new another component dedicated to passwords
-  // TODO: To add more improved styles to displayed on focus
+
   return (
     <Fragment>
       <div
         className="relative pt-4 flex flex-col items-start 
-         justify-center gap-1 w-full"
+         justify-center gap-1 w-full text-gray-800"
       >
         {formik.errors[`${name}`] && formik.touched[`${name}`] && (
           <p className="absolute top-0 left-0 text-sm text-red-600">
             {formik.errors[`${name}`]}
           </p>
         )}
-        <label className="text-sm first-letter:uppercase">{label}</label>
+        <label className="text-sm first-letter:uppercase font-[500]">
+          {label}
+        </label>
         <input
           type={props.type}
           id={name}
@@ -33,8 +34,10 @@ export const InputField: React.FC<InputFieldProps> = (props) => {
           onChange={formik.handleChange}
           value={formik.values[`${name}`]}
           className="p-2 outline-none rounded border-[1px]
-           border-gray-500 focus:border-[2px] focus:border-primary
-           transition-all text-sm w-full"
+           border-gray-500 focus:border-[1px] focus:border-primary
+           transition-all text-sm w-full focus:outline-none 
+           focus:shadow-[0px_0px_0px_4px_rgba(28,126,214,0.3)]
+           text-gray-800"
         />
       </div>
     </Fragment>
