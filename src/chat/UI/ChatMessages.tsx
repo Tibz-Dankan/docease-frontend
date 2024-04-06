21354cec8f98ca95145870592c6bdd6c436ae084
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-04-06 14:12:21 +0300

    feat: attempt to mark message as read on scroll to the bottom

--- src/chat/UI/ChatMessages.tsx
+++ src/chat/UI/ChatMessages.tsx
@@ -3,10 +3,17 @@ import { MessagePrimary } from "./MessagePrimary";
 import { MessageSecondary } from "./MessageSecondary";
 import { useSelector } from "react-redux";
 import { MessageDay } from "./MessageDay.tsx";
-import { IChatMessage, IOrganizedChatMessage } from "../../types/chat.ts";
+import {
+  IChatMessage,
+  IOrganizedChatMessage,
+  // TChatRecipient,
+  TChatState,
+} from "../../types/chat.ts";
 import { TUser } from "../../types/auth.ts";
 import Messages from "../../utils/organizeMessages.ts";
 import { MessagePlaceholder } from "./MessagePlaceholder.tsx";
+// import { markMessagesAsRead } from "../API/index.ts";
+// import { updateMessagesAsRead } from "../../store/actions/chat.ts";
 
 interface ChatMessagesProps {
   messages: IChatMessage[];
@@ -14,9 +21,15 @@ interface ChatMessagesProps {
 
 export const ChatMessages: React.FC<ChatMessagesProps> = (props) => {
   const currentUser: TUser = useSelector((state: any) => state.auth.user);
-  const recipient: TUser = useSelector(
-    (state: any) => state.chat.currentRecipient
+  const recipient = useSelector(
+    (state: TChatState) => state.chat.currentRecipient
   );
+
+  // const accessToken = useSelector(
+  //   (state: TAuthState) => state.auth.accessToken!
+  // );
+
+  // const dispatch: any = useDispatch();
 
   const messages = new Messages(currentUser, recipient).organize(
     props.messages
@@ -43,6 +56,51 @@ export const ChatMessages: React.FC<ChatMessagesProps> = (props) => {
     if (!message.isPrimaryMessage) return true;
     return false;
   };
+
+  // const markMessagesAsReadHandler = async () => {
+  //   const currentUserId = currentUser.userId!;
+
+  //   const unReadMessage = recipient.messages.find((message) => {
+  //     return message.isRead === false && message.recipientId === currentUserId;
+  //   });
+  //   if (!unReadMessage) return;
+
+  //   const lastMessageCreatedAt =
+  //     recipient.messages[recipient.messages.length - 1].createdAt;
+
+  //   if (!lastMessageCreatedAt || !currentUserId) return;
+
+  //   await markMessagesAsRead({
+  //     userId: currentUserId,
+  //     createdAt: lastMessageCreatedAt,
+  //     accessToken: accessToken,
+  //   });
+  //   dispatch(updateMessagesAsRead(currentUserId, recipient.userId));
+  // };
+
+  // useEffect(() => {
+  //   const scrollToBottom = () => {
+  //     // Delay scrolling to bottom to allow element attain its full height
+  //     setTimeout(() => {
+  //       const viewElement = document.querySelector("#message-container")!;
+  //       // viewElement.scrollIntoView({ behavior: "smooth" });
+  //       // viewElement.scrollTop = viewElement?.scrollHeight;
+
+  //       // Check if scrolled to the bottom
+  //       const isScrolledToBottom =
+  //         viewElement.scrollTop >=
+  //         viewElement.scrollHeight - viewElement.clientHeight;
+
+  //       if (!isScrolledToBottom) return;
+  //       // Mark messages as read if any on reaching the bottom
+  //       viewElement.addEventListener("scrollend", (_) => {
+  //         markMessagesAsReadHandler();
+  //       });
+  //     }, 50);
+  //   };
+
+  //   scrollToBottom();
+  // }, []);
 
   return (
     <Fragment>
