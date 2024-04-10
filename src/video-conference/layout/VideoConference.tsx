4136e69e94502e8424924b465e2e1a7315322936
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-04-10 11:59:29 +0300

    refactor: switch to sockets for video conf

--- src/video-conference/layout/VideoConference.tsx
+++ src/video-conference/layout/VideoConference.tsx
@@ -5,21 +5,21 @@ import {
   TVideoConferenceConnected,
   TVideoConferenceState,
 } from "../../types/videoConference";
-import { TAuthState } from "../../types/auth";
+import { TAuthState, TUser } from "../../types/auth";
 import { IoVideocam, IoVideocamOff } from "react-icons/io5";
 import { AiFillAudio, AiOutlineAudioMuted } from "react-icons/ai";
 import { MdCallEnd } from "react-icons/md";
 import { IconContext } from "react-icons";
 import { JoinVideoConference } from "../UI/JoinVideoConference";
+import { Socket, io } from "socket.io-client";
+import { generateChatRoomId } from "../../utils/generateChatRoomId";
 
 export const VideoConference: React.FC = () => {
-  const myVideoStreamRef = useRef<MediaStream>();
-  const peerRef = useRef<Peer>();
-  const [peerId, setPeerId] = useState<string>("");
   const [hasJoinedConference, setHasJoinedConference] =
     useState<boolean>(false);
   const [hasMutedVideo, setHasMutedVideo] = useState<boolean>(false);
   const [hasMutedAudio, setHasMutedAudio] = useState<boolean>(false);
+  const user = useSelector((state: TAuthState) => state.auth.user!);
 
   const joinedConferenceHandler = (hasJoined: boolean) => {
     setHasJoinedConference(() => hasJoined);
@@ -39,50 +39,33 @@ export const VideoConference: React.FC = () => {
     (state: TVideoConferenceState) => state.videoConference
   ) as TVideoConferenceConnected;
 
-  const userId = useSelector((state: TAuthState) => state.auth.user?.userId)!;
-
   console.log("videoConference: ", videoConference);
+  const socket = useRef<Socket | null>(io("http://localhost:3030"));
+  const peer = useRef<Peer | null>(null);
+  const effectRan = useRef(false);
 
   useEffect(() => {
-    const roomId = videoConference.videoConferenceId;
-    if (!roomId) return;
+    const initializeMediaStream = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true,
+        });
 
-    // Connect new user to conference call
-    const connectToNewUser = (userId: string, stream: MediaStream) => {
-      console.log("I call someone " + userId);
-      const call = peerRef.current!.call(userId, stream);
-      const videoElement = document.getElementById("host") as HTMLVideoElement;
-
-      call.on("stream", (userVideoStream) => {
-        addVideoStream(videoElement, userVideoStream);
-      });
-    };
-
-    const addVideoStream = (video: HTMLVideoElement, stream: MediaStream) => {
-      video.srcObject = stream;
-      video.addEventListener("loadedmetadata", () => {
-        video.play();
-      });
-    };
-
-    navigator.mediaDevices
-      .getUserMedia({ audio: true, video: true })
-      .then((stream) => {
-        myVideoStreamRef.current = stream;
-        const videoElement = document.getElementById(
-          "host"
+        const video = document.getElementById(
+          "current-user"
         ) as HTMLVideoElement;
-        addVideoStream(videoElement, stream);
+        addVideoStream(video, stream);
 
-        peerRef.current = new Peer({
-          // host: "/", //To be tested now
+        // TODO: To dynamically cater for the production host value
+        peer.current = new Peer({
           host: "127.0.0.1",
           port: 8000,
           path: "/peerjs",
           config: {
             iceServers: [
-              { url: "stun:stun01.sipphone.com" },
-              { url: "stun:stun.ekiga.net" },
+              { urls: "stun:stun01.sipphone.com" },
+              { urls: "stun:stun.ekiga.net" },
               { url: "stun:stunserver.org" },
               { url: "stun:stun.softjoys.com" },
               { url: "stun:stun.voiparound.com" },
@@ -105,41 +88,76 @@ export const VideoConference: React.FC = () => {
           debug: 3,
         });
 
-        // current user joins the call
-        peerRef.current.on("open", (id) => {
-          setPeerId(() => id);
-          console.log("my peerId " + id);
+        if (!peer.current) return;
+        console.log("socket.current:::", socket.current);
+        console.log("peer.current:::", peer.current);
 
-          // socket.current!.emit("join-room", videoConf);
+        peer.current.on("open", (peerId) => {
+          console.log("my id is" + peerId);
+
+          if (!socket.current?.connected) return;
+
+          const isCurrentUserHost = user.userId === videoConference.hostId;
+          const peerUser: TUser = isCurrentUserHost
+            ? videoConference.Attendee!
+            : videoConference.Host!;
+
+          const roomId = generateChatRoomId(user, peerUser);
+
+          console.log("Joining video conf chat room...");
+          socket.current?.emit("join-room", roomId, peerId, peerUser);
         });
 
-        // current user is being called
-        peerRef.current.on("call", (call) => {
+        // someone call me
+        peer.current.on("call", (call) => {
           console.log("someone call me");
           call.answer(stream);
-          const videoElement = document.getElementById(
-            "attendee"
+
+          const video = document.getElementById(
+            "peer-user"
           ) as HTMLVideoElement;
 
           call.on("stream", (userVideoStream) => {
-            addVideoStream(videoElement, userVideoStream);
+            addVideoStream(video, userVideoStream);
           });
         });
 
-        // // listen for joining user and them connect them to the call
-        // socket.current!.on("user-connected", (userId: string) => {
-        //   connectToNewUser(userId, stream);
-        // });
-        if (videoConference.hasConnected) {
-          connectToNewUser(videoConference.connectPeerId, stream);
-        }
-      })
-      .catch((error) => console.error("Error accessing media devices:", error));
-
-    return () => {
-      // peerRef && peerRef?.current!.disconnect();
+        socket.current?.on("user-connected", (peerId: string) => {
+          console.log("New user connected with peerId::::=>", peerId);
+          connectToNewUser(peerId, stream);
+        });
+      } catch (error) {
+        console.error("Error accessing media devices:", error);
+      }
     };
-  }, [userId]);
+
+    const addVideoStream = (video: HTMLVideoElement, stream: MediaStream) => {
+      video.srcObject = stream;
+      video.addEventListener("loadedmetadata", () => {
+        video.play();
+      });
+    };
+
+    // I call someone
+    const connectToNewUser = (peerId: string, stream: MediaStream) => {
+      console.log("I call someone" + peerId);
+      const call = peer.current?.call(peerId, stream);
+      if (call) {
+        const video = document.getElementById("peer-user") as HTMLVideoElement;
+
+        call.on("stream", (userVideoStream) => {
+          addVideoStream(video, userVideoStream);
+        });
+      }
+    };
+
+    if (effectRan.current === false) {
+      initializeMediaStream();
+      return () => {
+        effectRan.current = true;
+      };
+    }
+  }, [user]);
 
   return (
     <div className="w-full bg-green-500 min-h-[80vh]">
@@ -147,12 +165,12 @@ export const VideoConference: React.FC = () => {
         <div className="w-full flex-1 bg-blue-500 relative">
           <video
             className="w-full h-full bg-gray-300 rounded-md"
-            id="attendee"
+            id="peer-user"
           ></video>
           <video
             className="w-[20%] h-[20%] absolute bottom-4
              right-4 bg-gray-300 rounded-md shadow-md"
-            id="host"
+            id="current-user"
           ></video>
         </div>
         <div className="px-4 py-4 flex items-center justify-between bg-blue-600">
@@ -160,7 +178,7 @@ export const VideoConference: React.FC = () => {
             {!hasJoinedConference && (
               <JoinVideoConference
                 videoConferenceId={videoConference.videoConferenceId}
-                peerId={peerId}
+                peerId={"peerId"}
                 onJoin={joinedConferenceHandler}
               />
             )}
